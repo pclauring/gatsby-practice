13f22cc07ddaa36df81efd146e9836f8058ac330
Pierce Lauring <pclauring@gmail.com>
2019-05-08 22:39:39 -0400

    Test pages added with menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Tester blog</h1>
+    <p>Pierce's practice Gatsby Blog</p>
+    <p>React & Gatsby</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
